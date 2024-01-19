740a84c1a76e77875f944edcb3ee65c47909e321
Ponte, Nadia <deponte.work@gmail.com>
2024-01-19 02:20:26 +0000

    Added description and table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  
+  
+
+
+  // Combine all sections into the final markdown
+  return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [User Story](#userStory)
+- [Installation](#installation)
+- [Screenshots](#screenshots)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Deployment](#deployment)
+- [How to Contribute](#contribute)
+  - [Contributions](#contributions)
+  - [Starred Project](#starred)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+- [Badges](#badges)
+
+
+
+
+
+
+
 
 `;
 }
