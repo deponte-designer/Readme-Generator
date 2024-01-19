bb9cf6b2c06ffe4f28fcbbe4618c36cd1ee48807
Ponte, Nadia <deponte.work@gmail.com>
2024-01-19 02:28:47 +0000

    wrote first 3 questions: title, description and user story

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = ([
+    {
+        type: 'input',
+        name: 'title',
+        message: "What's your project's title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Briefly describe your project.",
+    },
+    {
+        type: "input",
+        name: "userStory",
+        message: "Summarise the user story."
+    },
 
-];
+
+]);
 
 // function to write README file
 function writeToFile(fileName, data) {
